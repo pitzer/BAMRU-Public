7f4242ce5010831c3f4fbb9a020355156063ad67
andyl <andy@r210.com>
2011-01-05 14:07:49 -0800

    added factories for test data generation

--- db/seed.rb
+++ db/seed.rb
@@ -3,12 +3,12 @@ puts "Wiping out old data"
 Event.all.each {|e| e.destroy}
 
 puts "Creating new data"
-20.times { Factory(:event, :kind => "meeting")                            }
-20.times { Factory(:event, :kind => "training")                           }
-20.times { Factory(:event, :kind => "event")                              }
-20.times { Factory(:event, :kind => "non county meeting")                 }
+20.times { Factory(:event, :kind => "meeting")                    }
+20.times { Factory(:event, :kind => "training")                   }
+20.times { Factory(:event, :kind => "event")                      }
+20.times { Factory(:event, :kind => "non_county")                 }
 
-10.times { Factory(:event, :kind => "meeting",            :end => randE ) }
-10.times { Factory(:event, :kind => "training",           :end => randE ) }
-10.times { Factory(:event, :kind => "event",              :end => randE ) }
-10.times { Factory(:event, :kind => "non county meeting", :end => randE ) }
+10.times { Factory(:event, :kind => "meeting",    :end => randE ) }
+10.times { Factory(:event, :kind => "training",   :end => randE ) }
+10.times { Factory(:event, :kind => "event",      :end => randE ) }
+10.times { Factory(:event, :kind => "non_county", :end => randE ) }
