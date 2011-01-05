0c30ac0f3dee492ed4c33c6a0dea734c437e43c1
andyl <andy@r210.com>
2011-01-05 09:18:59 -0800

    improved seed data

--- db/factories.rb
+++ db/factories.rb
@@ -1,14 +1,17 @@
 def rand2() srand.to_s[0..1]; end
 def rand3() srand.to_s[0..2]; end
 def rand4() srand.to_s[0..3]; end
+def randM() 1 + rand(12); end
+def randD() 1 + rand(28); end
+def randE() "2011-#{randM}-#{randD}"; end
 
 Factory.define :event do |u|
-  u.kind        "Normal" 
+  u.kind        "event" 
   u.title       { "U#{rand4}" } 
   u.location    { "L#{rand4}" } 
-  u.leaders     "hi"
-  u.start
+  u.leaders     { "Joe#{rand4}" }
+  u.start       { randE }
   u.end
-  u.description "Big Hello"
+  u.description { "Big Hello #{rand4}" }
 end
 
--- db/seed.rb
+++ db/seed.rb
@@ -3,7 +3,12 @@ puts "Wiping out old data"
 Event.all.each {|e| e.destroy}
 
 puts "Creating new data"
-e1 = Factory(:event, :kind => "meeting")
-e2 = Factory(:event, :kind => "training")
-e3 = Factory(:event, :kind => "event")
-e4 = Factory(:event, :kind => "non county meeting")
+20.times { Factory(:event, :kind => "meeting")                            }
+20.times { Factory(:event, :kind => "training")                           }
+20.times { Factory(:event, :kind => "event")                              }
+20.times { Factory(:event, :kind => "non county meeting")                 }
+
+10.times { Factory(:event, :kind => "meeting",            :end => randE ) }
+10.times { Factory(:event, :kind => "training",           :end => randE ) }
+10.times { Factory(:event, :kind => "event",              :end => randE ) }
+10.times { Factory(:event, :kind => "non county meeting", :end => randE ) }
