23907f87f777942bcea86611dd68a1e4ab62eb55
andyl <andy@r210.com>
2011-01-04 21:14:49 -0800

    added quotes and side-text

--- app.rb
+++ app.rb
@@ -6,8 +6,10 @@ require 'ruby-debug'
 class BamruApp < Sinatra::Base
 
   configure do
-    BASE_DIR = File.dirname(File.expand_path(__FILE__))
-    QUOTES   = YAML.load_file(BASE_DIR + "/quotes.yaml")
+    BASE_DIR  = File.dirname(File.expand_path(__FILE__))
+    QUOTES    = YAML.load_file(BASE_DIR + "/quotes.yaml")
+    RIGHT_NAV = YAML.load_file(BASE_DIR + "/right_nav.yaml")
+    GUEST_POLICY = File.read(BASE_DIR + "/guest_policy.html")
   end
 
   helpers do
@@ -16,17 +18,28 @@ class BamruApp < Sinatra::Base
     end
 
     def quote
-      idx = rand(QUOTES.length)
+      index = rand(QUOTES.length)
       <<-HTML
+        <br/><p/>
+        <img src="assets/axe.gif" border="0">
         <div class='quote_box'>
-          <div class='quote'>"#{QUOTES[idx][:text]}"</div>
-          <div class='caps'>- #{QUOTES[idx][:auth]}</div>
+          <div class='quote'>"#{QUOTES[index][:text]}"</div>
+          <div class='caps'>- #{QUOTES[index][:auth]}</div>
         </div>
       HTML
     end
 
     def blog_url
       "http://bamru.blogspot.com"
+    end
+    
+    def right_link(target, label)
+      "<a href='#{target}' class='nav3' onfocus='blur();'>#{label}</a><br/>"
+    end
+    
+    def right_nav(page)
+#      debugger
+      RIGHT_NAV[page].reduce("") {|a, v| a << right_link(v.last, v.first)} unless RIGHT_NAV[page].nil?
     end
 
     def menu_link(target, label)
@@ -46,58 +59,66 @@ class BamruApp < Sinatra::Base
   end
   
   get '/calendar' do
-    @title    = "BAMRU Calendar"
-    @hdr_img  = "images/mtn.jpg"
-    @side_txt = "TBD"
+    @title     = "BAMRU Calendar"
+    @hdr_img   = "images/mtn.jpg"
+    @right_nav = right_nav(:calendar)
+    @right_txt = GUEST_POLICY
+    @left_txt  = quote
     erb :calendar
   end
 
   get '/bamruinfo' do
-    @title    = "Information about BAMRU"
-    @hdr_img  = "images/approach.jpg"
-    @side_txt = "TBD"
+    @title     = "Information about BAMRU"
+    @hdr_img   = "images/approach.jpg"
+    @right_nav = right_nav(:bamruinfo)
+    @left_txt  = quote
     erb :bamruinfo
   end
 
   get '/join' do
-    @title    = "Joining BAMRU"
-    @hdr_img  = "images/helo.jpg"
-    @side_txt = "TBD"
+    @title     = "Joining BAMRU"
+    @hdr_img   = "images/helo.jpg"
+    @right_nav = right_nav(:join)
+    @right_txt = quote
     erb :join
   end
 
   get '/sgallery' do
-    @title    = "BAMRU Photo Gallery"
-    @hdr_img  = "images/hills.jpg"
-    @side_txt = "TBD"
+    @title     = "BAMRU Photo Gallery"
+    @hdr_img   = "images/hills.jpg"
+    @right_nav = right_nav(:sgallery)
     erb :sgallery
   end
 
   get '/meeting_locations' do
-    @title    = "BAMRU Meeting Location"
-    @hdr_img  = "images/mtn_2.jpg"
-    @side_txt = "TBD"
+    @title     = "BAMRU Meeting Location"
+    @hdr_img   = "images/mtn_2.jpg"
+    @right_nav = right_nav(:meeting_locations)
+    @right_txt = quote
     erb :meeting_locations
   end
 
   get '/sarlinks' do
-    @title    = "Links to SAR-related sites"
-    @hdr_img  = "images/glacier.jpg"
-    @side_txt = "SEARCH & RESCUE LINKS"
+    @title     = "Links to SAR-related sites"
+    @hdr_img   = "images/glacier.jpg"
+    @right_nav = right_nav(:sarlinks)
+    @right_txt = quote
     erb :sarlinks
   end
 
   get '/donate' do
-    @title    = "Donate to BAMRU"
-    @hdr_img  = "images/glacier.jpg"
-    @side_txt = @title
+    @title     = "Donate to BAMRU"
+    @hdr_img   = "images/glacier.jpg"
+    @right_nav = right_nav(:donate)
+    @right_txt = quote
     erb :donate
   end
 
   get '/contact' do
-    @title    = "BAMRU Contacts"
-    @hdr_img  = "images/HawthornLZ.jpg"
-    @side_txt = "CONTACTS"
+    @title     = "BAMRU Contacts"
+    @hdr_img   = "images/HawthornLZ.jpg"
+    @right_nav = right_nav(:contact)
+    @right_txt = quote
     erb :contact
   end
 
