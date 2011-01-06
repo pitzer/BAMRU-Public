c11fa373fcfa7c0caef2018bd663356613438f93
andyl <andy@r210.com>
2011-01-06 08:30:22 -0800

    generate fewer events in seed data

--- db/seed.rb
+++ db/seed.rb
@@ -3,10 +3,10 @@ puts "Wiping out old data"
 Event.all.each {|e| e.destroy}
 
 puts "Creating new data"
-10.times { Factory(:event, :kind => "meeting")                   }
-7.times { Factory(:event, :kind => "training")                   }
-8.times { Factory(:event, :kind => "event")                      }
-3.times { Factory(:event, :kind => "non_county")                 }
+10.times { Factory(:event, :kind => "meeting")                    }
+7.times  { Factory(:event, :kind => "training")                   }
+8.times  { Factory(:event, :kind => "event")                      }
+3.times  { Factory(:event, :kind => "non_county")                 }
 
-3.times { Factory(:event, :kind => "training",   :end => randE ) }
-2.times { Factory(:event, :kind => "event",      :end => randE ) }
+3.times  { Factory(:event, :kind => "training",   :end => randE ) }
+2.times  { Factory(:event, :kind => "event",      :end => randE ) }
