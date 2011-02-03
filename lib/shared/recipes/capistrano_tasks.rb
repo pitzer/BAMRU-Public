37bb2c549dbe9d9740607d322462e65d0d310eee
andyl <andy@r210.com>
2011-02-02 17:02:34 -0800

    deploy tweak

--- lib/shared/recipes/capistrano_tasks.rb
+++ lib/shared/recipes/capistrano_tasks.rb
@@ -20,6 +20,7 @@ desc "Update gem installation."
 task :update_gems do
   system "bundle pack"
   system "cd vendor ; rsync -a --delete cache #{SERVER}:a/#{APPDIR}/shared"
+  run "gem install rspec"
   run "cd #{current_path} ; bundle install --quiet --local --path=/home/aleak/.gems"
 end
 
