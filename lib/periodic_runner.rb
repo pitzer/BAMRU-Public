13b94ae987a2bb77f430c01bac2a5d9793e68a63
andyl <andy@r210.com>
2011-04-28 12:08:15 -0700

    periodic_runner work in progress

--- lib/periodic_runner.rb
+++ lib/periodic_runner.rb
@@ -1,21 +1,94 @@
+require 'yaml'
+
 class PeriodicRunner
-  def initialize(delay_in_seconds, &block)
-    @block = block
-    @delay_in_seconds = delay_in_seconds
+
+  attr_accessor :delay_seconds, :pid, :name, :code
+
+  def initialize(params = {}, &code)
+    remove_state_file
+    set_params(params, code)
   end
 
-  def start(&block)
+  def self.create(params = {}, &code)
+    set_params(params, code)
+    return self
+  end
+
+  def start(params = {}, &code)
+    set_params(params, code)
     puts "Starting Background Process"
-    pid = fork do
+    @pid = fork do
       loop do
-        value = yield
-        wait @delay_in_seconds
+        @code.call
+        sleep @delay_seconds
       end
     end
     Process.detach(pid)
+    save
     pid
   end
 
+  def restart(params = {}, &code)
+    set_params(params, code)
+    stop
+    start
+  end
 
+  def stop
+    Process.kill("SIGKILL", @pid)
+  end
+
+  def running?
+    `ps #{@pid} | grep #{@pid}`.empty?
+  end
+
+  def seconds_from_last_execution
+  end
+
+  def seconds_till_next_execution
+  end
+
+  def state_file
+    return "" if @name.blank?
+    name = "#{state_dir}/#{@name}.yaml"
+  end
+
+  private
+
+  def set_params(params, code)
+    new_params     = read_params_from_yaml_file.merge(params)
+    @code          = code || new_params[:code]  || nil
+    @delay_seconds = new_params[:delay_seconds] || 0
+    @name          = new_params[:name]          || ""
+    save
+  end
+
+  def params_hash
+    {
+      :pid           => @pid || nil,
+      :code          => @code,
+      :name          => @name,
+      :delay_seconds => @delay_seconds
+    }
+  end
+
+  def state_dir
+    "/tmp/periodic_runner"
+  end
+
+  def remove_state_file
+    system "rm -f #{state_file}" unless state_file.empty?
+  end
+
+  def read_params_from_yaml_file
+    return {} unless File.exist?(state_file)
+    YAML.load(File.read(state_file))
+  end
+
+  def save
+    return "" if @name.empty?
+    system "mkdir -p #{state_dir}"
+    File.open(state_file, 'w') {|f| f.puts YAML.dump(params_hash)} unless state_file.empty?
+  end
 
 end
--- spec/models/periodic_runner_spec.rb
+++ spec/models/periodic_runner_spec.rb
@@ -1,30 +1,84 @@
 require 'spec_helper'
 
-
-describe PeriodicRunnerBlock do
-  it "doesn't run for a public site"
-end
-
 describe PeriodicRunner do
-  it "has a status (on/off)"
-  it "doesn't run if peer isn't reachable"
-  it "runs every 15 minutes"
-  it "has a start method"
-  it "has a stop method"
-  it "has a configurable timer"
-  it "has a configurable executable"
-  it "overwrites data"
-  it "doesn't run if data is identical"
-  it "generates version history"
-  it "only allows one background process at a time"
-  it "has a PID attribute"
-  it "tells if the background process is running or not"
-  it "tells how much time is left until the next execution"
-  it "tells how much time has elapsed since the last execution"
-  it "is called from the settings object"
-  it "remembers its state if the server restarts"
-  it "restarts the task after it times out"
-  it "sends alert message if process term status is not nil"
-  it "doesn't leave zombie processes if the parent dies"
-  it "does not run if the parent dies"
+
+  describe "instance attributes" do
+    before(:each) { @obj = PeriodicRunner.new }
+    specify { @obj.should respond_to(:pid)             }
+    specify { @obj.should respond_to(:name)            }
+    specify { @obj.should respond_to(:code)            }
+    specify { @obj.should respond_to(:delay_seconds)   }
+  end
+
+  describe "instance methods" do
+    before(:each) { @obj = PeriodicRunner.new }
+    specify { @obj.should respond_to(:start)                        }
+    specify { @obj.should respond_to(:stop)                         }
+    specify { @obj.should respond_to(:restart)                      }
+    specify { @obj.should respond_to(:running?)                     }
+    specify { @obj.should respond_to(:state_file)                   }
+    specify { @obj.should respond_to(:seconds_till_next_execution)  }
+    specify { @obj.should respond_to(:seconds_from_last_execution)  }
+  end
+
+  context "basic object creation" do
+    context "with no params or code" do
+      before(:each) { @obj = PeriodicRunner.new }
+      specify { @obj.should_not be_nil          }
+      specify { @obj.name.should be_empty       }
+      specify { @obj.state_file.should be_empty }
+    end
+    context "with a name attribute" do
+      before(:each) { @obj = PeriodicRunner.new(:name => "hi") }
+      specify { @obj.should_not be_nil                         }
+      specify { @obj.name.should == "hi"                       }
+      specify { @obj.state_file.should_not be_empty            }
+      specify { File.exist?(@obj.state_file).should be_true    }
+    end
+    context "with a code block" do
+      before(:each) { @obj = PeriodicRunner.new {puts 'hi'} }
+      specify { @obj.should_not be_nil                         }
+      specify { @obj.code.should_not be_nil                    }
+    end
+  end
+
+  context "create" do
+    it "reloads" do
+      @obj1 = PeriodicRunner.new :name => "test1", :delay_seconds => 33
+      @obj2 = PeriodicRunner.create :name => "test1"
+      @obj2.delay_seconds.should == 33
+    end
+  end
+
+#  context "running" do
+#    before(:each) do
+#      @obj = PeriodicRunner.new
+#      @obj.delay_seconds = 5
+#      @obj.name          = "test1"
+#      @obj.start { x = 1 }
+#    end
+#    specify { @obj.pid.should_not == nil }
+#  end
+
+#  it "only allows one background process at a time"
+#  it "is called from the settings object"
+#  it "remembers its state if the server restarts"
+#  it "restarts the task after it times out"
+#  it "sends alert message if process term status is not nil"
+#  it "doesn't leave zombie processes if the parent dies"
+#  it "stops running if the parent dies"
+
 end
+
+
+#describe Settings do
+##  it "has a status (on/off)"
+#end
+#
+#describe PeriodicBlock do
+##  it "doesn't run for a public site"
+##  it "doesn't run if peer isn't reachable"
+##  it "overwrites data"
+##  it "doesn't run if data is identical"
+##  it "generates version history"
+#end
