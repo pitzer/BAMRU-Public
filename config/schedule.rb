f47edefaabe412587f79d7c361ec03d459edc4a2
andyl <andy@r210.com>
2011-12-19 10:06:39 -0800

    updated gcal_sync script

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,23 +1,4 @@
-# Use this file to easily define all of your cron jobs.
-#
-# It's helpful, but not entirely necessary to understand cron before proceeding.
-# http://en.wikipedia.org/wiki/Cron
 
-# Example:
-#
-# set :output, "/path/to/my/cron_log.log"
-#
-# every 2.hours do
-#   command "/usr/bin/some_great_command"
-#   runner "MyModel.some_method"
-#   rake "some:great:rake:task"
-# end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
-
-# Learn more: http://github.com/javan/whenever
 #
 dir = File.dirname(File.expand_path(__FILE__))
 
@@ -25,16 +6,12 @@ env :RACK_ENV, 'production'
 
 set :output, {:standard => 'log/cron_normal.log', :error => 'log/cron_error.log'}
 
-#every 20.minutes do
-#  command "cd #{dir} && bundle exec rake data_import"
-#end
-
 every 12.hours do
   command "cd #{dir} && mv log/cron_normal.log log/cron_normal_backup.log"
   command "cd #{dir} && mv log/cron_error.log log/cron_error_backup.log"
 end
 
 every 1.day do
-  command "cd #{dir} && bundle exec rake gcal_sync"
+  command "cd #{dir} && bundle exec rake gcal_sync RACK_ENV=production"
 end
 
