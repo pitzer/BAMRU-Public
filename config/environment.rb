62c10b2e080d7b6b1c772fe031bf0c128cc332be
andyl <andy@r210.com>
2012-07-23 10:12:56 -0700

    updated development database

--- config/environment.rb
+++ config/environment.rb
@@ -5,26 +5,28 @@ require 'active_support/core_ext'
 # require 'factory_girl'
 require 'time'
 
-BASE_DIR  = File.dirname(File.expand_path(__FILE__)) + "/../"
+BASE_DIR  = File.expand_path('../', File.dirname(__FILE__))
 DATA_DIR  = BASE_DIR + "/data"
 
 INVAL_CSV_FILENAME = "/tmp/inval_csv.csv"
 INVAL_REC_FILENAME = "/tmp/inval_rec.csv"
 
-database_file = case ENV['RACK_ENV']
+DATABASE_FILE = case ENV['RACK_ENV']
   when "production" then "production.sqlite3"
   when "test"       then "test.sqlite3"
   else "database.sqlite3"
 end
 
+DATABASE_SPEC = BASE_DIR + "/db/#{DATABASE_FILE}"
+
 # initialize the database
 ActiveRecord::Base.establish_connection(
   :adapter => 'sqlite3',
-  :database =>  BASE_DIR + "db/#{database_file}"
+  :database => DATABASE_SPEC
 )
 
 # load all lib
-Dir[BASE_DIR + "lib/*.rb"].each {|f| load f}
+Dir[BASE_DIR + "/lib/*.rb"].each {|f| load f}
 
 # load factory definitions
 # require BASE_DIR + "db/factories"
--- lib/env_settings.rb
+++ lib/env_settings.rb
@@ -6,7 +6,7 @@ EOF
 
 env_settings.each_line do |val|
   constant = val.chomp.strip
-  eval "#{constant} = ENV['#{constant}']"
+  eval "#{constant} ||= ENV['#{constant}']"
   abort "ERROR: Missing Environment Value (#{constant})" if constant.nil?
 end
 
