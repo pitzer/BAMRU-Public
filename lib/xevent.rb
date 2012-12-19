16df5429c8c956d5923c7e35a5c6d5d895f29620
andyl <andy@r210.com>
2012-12-19 15:17:06 -0800

    fixed kml bug

--- lib/xevent.rb
+++ lib/xevent.rb
@@ -267,8 +267,7 @@ class Xevent < ActiveRecord::Base
   def kml_description
     lbl = leaders.include?(",") ? "Leaders" : "Leader"
     ldr = leaders == "TBA" ? "" : "#{lbl}: #{leaders}<br/>"
-    link = description.gsub("[","").gsub("]", "")
-    mesg = if match = description.match(/\[(.*)\]/)
+    mesg = if (match = description.match(/\[(http.*)\]/))
       link = match[1]
       "Find more information on the <a href='#{link}'>BAMRU Blog</a>"
            else
